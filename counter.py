51ca27f990b6d72bdd716d962266ada21d68abfb
Ankita17Talwar <talwar17ankita@gmail.com>
2021-11-04 22:31:31 +0530

    wrap wrapper with calling function

--- counter.py
+++ counter.py
@@ -1,5 +1,9 @@
 # how many times each of the functions in it gets called
+from functools import wraps
+
+
 def counter(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         wrapper.count += 1
         # Call the function being decorated and return the result
@@ -13,6 +17,9 @@ def counter(func):
 # Decorate foo() with the counter() decorator
 @counter
 def foo():
+    """
+    Function Foo
+    """
     print('calling foo()')
 
 
@@ -20,3 +27,4 @@ foo()
 foo()
 
 print('foo() was called {} times.'.format(foo.count))
+print(foo.__doc__)
